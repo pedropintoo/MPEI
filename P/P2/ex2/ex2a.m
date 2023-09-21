5ad4371bd7306418bfb120fe5844c41bcbdcd182
Pedro Pinto <pmap@ua.pt>
2023-09-21 15:42:02 +0100

    Commit P2

---
+++ P/P2/ex2/ex2a.m
@@ -0,0 +1,18 @@
+% Nao interessa a ordem!
+m = 100; % Alvos
+n = 20; % Dardos
+
+N = 1e6;
+
+acertos = randi(m, [n, N]);
+sucessos = 0;
+for i = 1:N
+    if(length(unique(acertos(:,i))) == n)
+        sucessos = sucessos + 1;
+    end
+
+end
+
+prob = sucessos / N;
+disp(prob)
+
